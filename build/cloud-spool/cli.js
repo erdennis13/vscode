cf93ed9b11feccafe8fdf6c69bf6887ca86db0a2
Ethan Dennis <erdennis13@gmail.com>
2018-10-12 09:24:37 -0400

    added cloudspool scripts

---
+++ build/cloud-spool/cli.js
@@ -0,0 +1,102 @@
+#!/usr/bin/env node
+
+const cache = require('./index.js');
+const fs = require('fs');
+const pkg = require('./package.json');
+
+const [,, ...args] = process.argv;
+
+let cmd;
+let target;
+let hash;
+let file;
+let sas;
+
+for (let i = 0; i < args.length; i++) {
+  let a = args[i];
+  switch (a) {
+    case 'help':
+      cmd = runHelp;
+      break;
+    case 'cache':
+      cmd = runCache;
+      break;
+    case 'retrieve':
+      cmd = runRetrieve;
+      break;
+    case '-h':
+      hash = args[++i];
+      break;
+    case '-f':
+      file = args[++i];
+      break;
+    case '-sas':
+      sas = args[++i];
+      break;
+    default:
+      if (!cmd) {
+        fail(`Invalid command "${a}"`);
+      } else if (target) {
+        fail(`Invalid argument "${a}"`);
+      }  else {
+        target = a;
+      }
+      break;
+  }
+}
+
+target = target || process.cwd();
+cmd = cmd || runHelp;
+
+if ((cmd !== runHelp) && (!(hash || file) || (hash && file))) {
+  // if not help command and (neither defined or both defined)
+  fail('Must specify hash key (-h) or file to hash (-f)');
+}
+
+cmd();
+
+async function runCache() {
+  if (!fs.existsSync(target)) {
+    fail('Source path does not exist');
+  }
+
+  if (hash) {
+    await cache.uploadToCacheByHash(hash, target);
+  } else if (file) {
+    await cache.uploadToCache(file, target);
+  }
+}
+
+async function runRetrieve() {
+  if (!fs.existsSync(target)) {
+    fail('Destination path does not exist');
+  }
+
+  if (!sas && !process.env.BLOBCACHEACCESSKEY) {
+    fail('Must provide a shared access signature (-sas) or set the BLOBCACHEACCESSKEY environment variable');
+  }
+
+  let success;
+  if (hash) {
+    success = await cache.downloadFromCacheByHash(hash, target, sas);
+  } else if (file) {
+    success = await cache.downloadFromCache(file, target, sas);
+  }
+
+  if (!success) {
+    console.error('Cache miss');
+  }
+}
+
+function runHelp() {
+  console.log(`${pkg.name} ${pkg.version}`);
+  console.log(`usage:
+    upload:      cache [source path] [-h <hash key>] [-f <file to hash>]
+    download:    retrieve [destination path] [-h <hash key>] [-f <file to hash>] [-sas <token>]`
+  );
+}
+
+function fail(message) {
+  console.log(message);
+  process.exit(1);
+}
---
+++ build/cloud-spool/index.js
@@ -0,0 +1,100 @@
+const cp = require('child_process');
+const crypto = require('crypto');
+const fs = require('fs');
+const path = require('path');
+const util = require('util');
+
+const spawn = cp.spawn;
+const exec = util.promisify(cp.exec);
+const readFile = util.promisify(fs.readFile);
+const stat = util.promisify(fs.stat);
+
+const CACHE_URL = 'https://cloudspool-dev.azurewebsites.net/api/vscodeyarncache/';
+
+exports.downloadFromCache = async function(hashSourcePath, artifactDestinationPath, sas) {
+  const hash = await exports.hashFile(hashSourcePath);
+  return await exports.downloadFromCacheByHash(hash, artifactDestinationPath, sas);
+}
+
+exports.downloadFromCacheByHash = async function(sourceHash, artifactDestinationPath, sas) {
+  const isWin = process.platform === "win32";
+  let resolvedDestinationPath = path.resolve(artifactDestinationPath);
+  
+  if (isWin) {
+    resolvedDestinationPath = '/' + resolvedDestinationPath.replace(":","").replace(/\\/g, "/");
+  }
+  
+  if (isWin) {
+    await stat(artifactDestinationPath); // ensure exists
+  } else {
+    await stat(resolvedDestinationPath); // ensure exists
+  }
+  
+  const url = CACHE_URL + sourceHash;
+  const { stdout: response } = await exec(`curl "${url}"`, {stdio: 'inherit'});
+
+  if (response) {
+    sas = sas || process.env.BLOBCACHEACCESSKEY;
+    if (!sas) {
+      throw new Error('SAS not provided and BLOBCACHEACCESSKEY environment variable not set');
+    }
+    
+    const blobUrl = JSON.parse(response) + sas;
+
+    const {stdout, stderr} = await exec(`curl -s "${blobUrl}" | tar xz -C "${resolvedDestinationPath}"`, {stdio: 'inherit'});
+    if (stderr) {
+      console.log(`error: ${stderr}`);
+      return false;
+    }
+    console.log(stdout);
+    return true;
+  }
+
+  return false;
+}
+
+exports.uploadToCache = async function(hashSourcePath, artifactSourcePath) {
+  const hash = await exports.hashFile(hashSourcePath);
+  return await exports.uploadToCacheByHash(hash, artifactSourcePath);
+}
+
+exports.uploadToCacheByHash = async function(sourceHash, artifactSourcePath) {
+  const isWin = process.platform === "win32";
+
+  let resolvedSourcePath = path.resolve(artifactSourcePath);
+  let sourceBasename = path.basename(resolvedSourcePath);
+  let parentPath = path.join(resolvedSourcePath, '..');
+  let tarballPath = path.join(parentPath, sourceHash + '.tar.gz');
+  let originalTarballPath = tarballPath;
+  let url = CACHE_URL + sourceHash;
+  
+  await stat(resolvedSourcePath); // ensure exists
+
+  if (isWin) {
+    resolvedSourcePath = '/' + resolvedSourcePath.replace(":","").replace(/\\/g, "/");
+    sourceBasename = sourceBasename.replace(":","").replace(/\\/g, "/");
+    parentPath = '/' + parentPath.replace(":","").replace(/\\/g, "/");
+    tarballPath = '/' + tarballPath.replace(":","").replace(/\\/g, "/");
+  }
+
+  let {stdout, stderr} = await exec(`tar -czf "${tarballPath}" -C "${parentPath}" "${sourceBasename}"`, {stdio: 'inherit'});
+  if (stderr) {
+    console.error(`${stderr}`);
+  }
+  if (stdout) {
+    console.log(`${stdout}`);
+  }
+  let {stdout2, stderr2} = await exec(`curl -X PUT -H "Content-Type: multipart/form-data" -F "file=@${originalTarballPath}" "${url}"`, {stdio: 'inherit'});
+  if (stderr2) {
+    console.error(`error: ${stderr2}`);
+  }
+  console.log(`${stdout2}`);
+  await exec(`rm -rf ${tarballPath}`);
+}
+
+exports.hashFile = async function(hashSourcePath) {
+  const resolvedSourcePath = path.resolve(hashSourcePath);
+  let fileContents = await readFile(resolvedSourcePath, 'utf8');
+  fileContents = fileContents.replace(/(\r|\n)/gm,"");
+  return crypto.createHash('sha256').update(fileContents).digest('hex') + "-" + process.platform;
+}
