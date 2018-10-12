ecf4b0135df28a955a59c3e67d22711d8c46f30f
Ethan Dennis <erdennis13@gmail.com>
2018-10-12 13:40:37 -0400

    Make yaml builds use custom yarn

--- scripts/yarn-custom.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-yarn () {
-	if [[ $@ == "install" ]]; then
-		command yarn install --pre-fetch-script "node $BUILD_SOURCESDIRECTORY/build/cloud-spool/cli.js retrieve -f ./yarn.lock" --post-install-script "node $BUILD_SOURCESDIRECTORY/build/cloud-spool/cli.js cache ./node_modules -f ./yarn.lock"
-	else
-		command yarn "@a"
-	fi
-}
