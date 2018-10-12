4769968ff49688d50f91c658a63288ea7038086a
Ethan Dennis <erdennis13@gmail.com>
2018-10-12 10:54:02 -0400

    use correct env variable for yarn alias

--- scripts/yarn-custom.sh
+++ scripts/yarn-custom.sh
@@ -2,7 +2,7 @@
 
 yarn () {
 	if [[ $@ == "install" ]]; then
-		command yarn install --pre-fetch-script "node $(Build.SourcesDirectory)/build/cloud-spool/cli.js retrieve -f ./yarn.lock" --post-install-script "node $(Build.SourcesDirectory)/build/cloud-spool/cli.js cache ./node_modules -f ./yarn.lock"
+		command yarn install --pre-fetch-script "node $BUILD_SOURCESDIRECTORY/build/cloud-spool/cli.js retrieve -f ./yarn.lock" --post-install-script "node $BUILD_SOURCESDIRECTORY/build/cloud-spool/cli.js cache ./node_modules -f ./yarn.lock"
 	else
 		command yarn "@a"
 	fi
